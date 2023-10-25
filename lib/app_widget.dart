b42594b7707147cf3b85e5be1681f74d75a9f734
Diogo Carvalho <diogobernardini2003@gmail.com>
2023-10-25 20:04:28 -0300

    #11 Tela inicial - Front End(em processo)

---
+++ lib/app_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:pi_app/home_page.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PI App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        brightness: Brightness.dark
+      ),
+      home: const HomePage(),
+    );
+  }
+}
